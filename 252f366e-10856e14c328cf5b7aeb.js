6e59d9f573118971a01c226502e67b0ca3210b6a
0jo-gil <dudwh921112@naver.com>
2024-04-12 17:06:51 +0900

    Updates

--- 252f366e-10856e14c328cf5b7aeb.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[228],{5434:function(t,a,e){e.d(a,{ixJ:function(){return n}});var r=e(4405);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"}}]})(t)}}}]);
-//# sourceMappingURL=252f366e-10856e14c328cf5b7aeb.js.map
